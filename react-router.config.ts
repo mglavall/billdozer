2c40ebf7c89d66823bfb139d3abaf043370c2acf
Marsa <mgomezlavall@gmail.com>
2025-11-29 18:26:20 +0100

    Remove preset vercel

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,12 +1,8 @@
 import type { Config } from "@react-router/dev/config";
-import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from "@react-router/vercel";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  presets: [vercelPreset()],
-} satisfies Config;
-ets: [vercelPreset()],
 } satisfies Config;
